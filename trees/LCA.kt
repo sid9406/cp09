c4b5697e4a0d201ca59e527dff709a58c9298fe8
sid9406 <sid.mishra74@gmail.com>
2020-11-30 17:25:43 +0530

    Update LCA.kt

--- trees/LCA.kt
+++ trees/LCA.kt
@@ -1,21 +1,21 @@
-class DFS(val n  : Int, val root : Int, val graph : MutableMap<Int, MutableList<Pair<Int, Int>>>){
+class LCA(val n: Int, val graph: MutableMap<Int, MutableList<Int>>) {
 
-    val sum = LongArray(n + 1){ 0L }
-    val levelArray  = IntArray(n + 1) { 0 }
-    val up = Array(n + 1) { IntArray(19) { -1 }}
+    val levelArray = IntArray(n + 1) { 0 }
+    val up = Array(n + 1) { IntArray(19) { -1 } }
 
     init {
-        dfs(root, -1)
+        dfs(1, -1)
         buildUp()
     }
 
-    fun dfs(root : Int, parent : Int){
-        graph[root]!!.forEach {
-            if (it.first != parent){
-                sum[it.first] = it.second + sum[root]
-                levelArray[it.first] = levelArray[root] + 1
-                up[it.first][0] = root
-                dfs(it.first, root)
+    fun dfs(root: Int, parent: Int) {
+        if (graph.containsKey(root)) {
+            graph[root]!!.forEach {
+                if (it != parent) {
+                    levelArray[it] = levelArray[root] + 1
+                    up[it][0] = root
+                    dfs(it, root)
+                }
             }
         }
     }
@@ -30,30 +30,43 @@ class DFS(val n  : Int, val root : Int, val graph : MutableMap<Int, MutableList<
         }
     }
 
+    fun distance(a: Int, b: Int): Int {
+        //calculates distance between node a and b
+        return levelArray[a] + levelArray[b] - 2 * levelArray[lca(a, b)]
+    }
 
-    fun lca(u: Int, v: Int): Int {
-        var u = u
-        var v = v
+    fun lift(node: Int, c: Int): Int {
+        //find the node which we get after lifting a by c
+        var cur = node
+        for (i in 0 until 19)
+            if (c shr i and 1 == 1)
+                cur = up[cur][i]
+
+        return cur
+    }
+
+
+    fun lca(uu: Int, vv: Int): Int { // 5 and 4
+        var u = uu
+        var v = vv
+
         if (levelArray[v] < levelArray[u]) {
-            u += v
-            v = u - v
-            u -= v
+            return lca(v, u)
         }
 
         val diff = levelArray[v] - levelArray[u]
 
-        for (i in 0 until 19)
-            if (diff shr i and 1 == 1)
-                v = up[v][i]
+        v = lift(v, diff)
 
         if (u == v)
             return u
 
-        for (i in 18 downTo 0)
+        for (i in 18 downTo 0) {
             if (up[u][i] != up[v][i]) {
                 u = up[u][i]
                 v = up[v][i]
             }
+        }
 
         return up[u][0]
     }
